4e521d64ebc3b687a9e8e3d0f063e765a294116b
Alex macbookAir <aleksandrmisnov@gmail.com>
2025-01-31 14:31:39 +0000

    first commit

---
+++ src/api/featured-project-donation/routes/featured-project-donation.ts
@@ -0,0 +1,7 @@
+/**
+ * featured-project-donation router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::featured-project-donation.featured-project-donation');
---
+++ src/api/featured-project-donation/services/featured-project-donation.ts
@@ -0,0 +1,7 @@
+/**
+ * featured-project-donation service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::featured-project-donation.featured-project-donation');
